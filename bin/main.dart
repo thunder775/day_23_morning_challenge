c844ef785e33a3257c4192b8bdb27e9118080320
McLarenCollege <hello@mclarencollege.in>
2019-11-04 08:26:36 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+// Challenge 1
+// Write test cases for the given challenges
+
+// Challenge - Conway's Game of Life
+// https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
+// The Game of Life, also known simply as Life, is a cellular automaton devised
+// by the British mathematician John Horton Conway in 1970.
+// Write a function which performs one step of the life algorithm
+
+main() {
+
+}
